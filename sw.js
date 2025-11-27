1aa395377ecfd664e038ea1ebd529947f9f593f0
vellumgracia-del <vellumgracia@gmail.com>
2025-11-27 20:59:11 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'kaspro-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdn.jsdelivr.net/npm/chart.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// Fetch Event (Network First, then Cache)
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => {
+            return caches.match(e.request);
+        })
+    );
+});
